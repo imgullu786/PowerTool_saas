7e27778f047828d40e4e9e96761348e1fbcb163b
Md Gulam Gaush <gulamgaush.dev@gmail.com>
2026-03-12 04:18:18 +0530

    add core doctypes

---
+++ power_tool_saas/power_tool_saas/doctype/pt_remote_archive_file/pt_remote_archive_file.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Md Gulam Gaush and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PT Remote Archive File", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ power_tool_saas/power_tool_saas/doctype/pt_site_subscription/pt_site_subscription.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Md Gulam Gaush and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PT Site Subscription", {
+// 	refresh(frm) {
+
+// 	},
+// });
